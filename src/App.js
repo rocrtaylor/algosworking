c641cd110a7beb29837f3b3376254cf0a12c5740
Roc Taylor <rocrtaylor@gmail.com>
2022-07-11 18:21:21 -0500

    initial push

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Algos from "./algos.json";
+
+function App() {
+  const algos = Algos;
+  const [show, setShow] = useState(false);
+  const [seeCode, setSeeCode] = useState(0);
+
+  const handleShow = (index) => {
+    setShow(!show);
+    setSeeCode(index);
+  };
+
+  return (
+    <>
+      <Header />
+
+      <div>
+        {algos.map((item, index) => {
+          return (
+            <div key={index} item={item} onClick={() => handleShow(index)}>
+              <h1 className="column">{item.name}</h1>
+
+              {show && seeCode === index ? (
+                <div className="column">
+                  <img src={item.icon} alt={"code"} />
+                </div>
+              ) : null}
+            </div>
+          );
+        })}
+      </div>
+      <Footer style={{ backgroundColor: "blue" }} />
+    </>
+  );
+}
+
+export default App;
